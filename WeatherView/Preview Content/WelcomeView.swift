5d445d34fd7d3d80c51238d0ccdd2d6937b9ee24
andreknvas <andreluiz1002@gmail.com>
2025-07-21 13:45:16 -0300

    first commit

---
+++ WeatherView/Preview Content/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  WeatherView
+//
+//  Created by Deko on 21/07/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}

81932bffdb22274e68ef6139fedc3693c97cde79
andreknvas <andreluiz1002@gmail.com>
2025-07-21 02:47:36 -0300

    Initial Commit

---
+++ WeatherView/WeatherViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewApp.swift
+//  WeatherView
+//
+//  Created by Deko on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherViewTests/WeatherViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewTests.swift
+//  WeatherViewTests
+//
+//  Created by Deko on 21/07/25.
+//
+
+import Testing
+@testable import WeatherView
+
+struct WeatherViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
